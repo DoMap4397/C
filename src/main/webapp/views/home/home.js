4aab222a9a340431b6f297aefdfa1d242d2463f7
Do <do.nguyen.4397@gmail.com>
2023-04-17 23:02:49 +0700

    home

---
+++ src/main/webapp/views/home/home.js
@@ -0,0 +1,29 @@
+import {withTranslation} from "react-i18next";
+import {Row, Col} from "reactstrap";
+import React from "react";
+
+const _ = require('lodash');
+
+// const openDetailForm = (options) => {
+//     showDialog(<DetailForm options={options}/>);
+// };
+
+const home = (props) => {
+    const {t} = props;
+
+    return(<Row xs={1} className="home-page">
+            <Col xs={6}>
+                <CommonButton>
+                    {/* onClick={()} */}
+                </CommonButton>
+            </Col>
+
+            <Col xs={6}>
+                <CommonButton>
+
+                </CommonButton>
+            </Col>
+    </Row>)
+}
+
+export default withTranslation(["Home", "common"])(home)
